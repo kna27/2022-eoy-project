8af2fa0b149112dbe059db07ea705866ff7a96ae
Krish Arora <43799189+kna27@users.noreply.github.com>
2022-05-24 08:35:33 -0400

    Add camera movement and export blend files to fbx

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 50f;
+    public float shiftMultiplier;
+    public float scrollSpeed = 250f;
+    public float rotateSpeed = 150f;
+
+    void FixedUpdate()
+    {
+
+        shiftMultiplier = Input.GetKey(KeyCode.LeftShift) ? 1.5f : 1f;
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * shiftMultiplier * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * moveSpeed * shiftMultiplier * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * moveSpeed * shiftMultiplier * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * moveSpeed * shiftMultiplier * Time.deltaTime);
+        }
+
+        GetComponent<Camera>().fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
+        GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, 30, 90);
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.eulerAngles += rotateSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
