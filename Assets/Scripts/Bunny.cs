5b65715ba10f751fc9c3462e0c6769be52c59ac3
Thogan24 <thogan1924@gmail.com>
2022-06-07 08:51:40 -0400

    Movement

---
+++ Assets/Scripts/Bunny.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class Bunny : MonoBehaviour
+{
+    public float waterWant;
+    public float foodWant;
+    public float reproductiveUrge;
+    public float lookRadius = 10f;
+    public float stoppingDistance = 5f;
+    public NavMeshAgent agent;
+    Transform target;
+    
+
+    void Start()
+    {
+        //target = ;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        
+        foodWant = Time.deltaTime * 3;
+        waterWant = Time.deltaTime * 5;
+        //if(transform == target)
+        //{
+            
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, lookRadius);
+            List<Collider> landColliders = new List<Collider>();
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                //Debug.Log(hitColliders[i].transform.name);
+                if (hitColliders[i].transform.gameObject.layer == LayerMask.NameToLayer("Land"))
+                {
+                    landColliders.Add(hitColliders[i]);
+                }
+            }
+
+            Debug.Log(landColliders.Count);
+            Random random = new Random();
+        //}
+        
+        //target =;
+        //float distance = Vector3.Distance(target.position, transform.position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
