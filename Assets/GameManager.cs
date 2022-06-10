45f2c407f4c52d3f6bbd569c06e933dc96ce0ede
kna27 <krisharora27@gmail.com>
2022-06-10 15:36:15 -0400

    UI overhaul

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,8 +5,14 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject statsPanel;
+    public GameObject controlsPanel;
     public float timeScale;
     public TextMeshProUGUI timeText;
+    public TextMeshProUGUI chickenPopulation;
+    public TextMeshProUGUI foxPopulation;
+    public TextMeshProUGUI food;
+    public int UIMode = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,13 +32,39 @@ public class GameManager : MonoBehaviour
         {
             timeScale -= 0.5f;
         }
-        if (Input.GetKeyDown (KeyCode.RightArrow) && timeScale < 10f)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && timeScale < 20f)
         {
             timeScale += 0.5f;
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UIMode += 1;
+            if (UIMode > 2)
+            {
+                UIMode = 0;
+            }
+        }
+        if (UIMode == 0)
+        {
+            statsPanel.SetActive(true);
+            controlsPanel.SetActive(true);
+        }
+        else if (UIMode == 1)
+        {
+            statsPanel.SetActive(true);
+            controlsPanel.SetActive(false);
+        }
+        else if (UIMode == 2)
+        {
+            statsPanel.SetActive(false);
+            controlsPanel.SetActive(false);
+        }
+
         Time.timeScale = pauseMenu.activeInHierarchy ? 0f : timeScale;
         timeText.text = "Speed: " + (timeScale * 100) + "%";
-        
+        chickenPopulation.text = "Chicken Population: " + GameObject.FindGameObjectsWithTag("Chicken").Length;
+        foxPopulation.text = "Fox Population: " + GameObject.FindGameObjectsWithTag("Fox").Length;
+        food.text = "Food: " + GameObject.FindGameObjectsWithTag("Food").Length;
     }
 
     public void Resume()
