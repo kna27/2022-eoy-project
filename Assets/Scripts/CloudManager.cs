83d41372489a68495c067d8e2e5e84e02398d789
Thogan24 <thogan1924@gmail.com>
2022-06-14 08:13:57 -0400

    Cloud

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -12,6 +12,6 @@ public class CloudManager : MonoBehaviour
     }
     public void InstantiateCloud()
     {
-        Instantiate(clouds[Random.Range(0, clouds.Length)], new Vector3(-30, 30, Random.value*-40), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        Instantiate(clouds[Random.Range(0, clouds.Length)], new Vector3(-30, 30, Random.value*-40), Quaternion.Euler(new Vector3(-90, Random.Range(0, 360), 0)));
     }
 }
