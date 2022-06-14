4e3161b981804d2c6429b98792ea0022af32e988
kna27 <krisharora27@gmail.com>
2022-06-13 20:48:05 -0400

    Add skybox + clouds

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    public GameObject[] clouds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
